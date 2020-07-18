2cb1401f36e062e800762c92e7658a313e84173a
dunganh23121995 <dungca2312@gmail.com>
2020-07-18 09:22:26 +0700

    18/7/2020

--- lib/publiccustom/card_show_product.dart
+++ lib/publiccustom/card_show_product.dart
@@ -3,44 +3,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ShowProduct extends StatelessWidget {
-  ShowProduct({this.color=Colors.white});
+  ShowProduct({this.color = Colors.white});
+
   Color color;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(
       children: <Widget>[
+        //Title
         Container(
           decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: <Color>[Colors.amber,Colors.deepOrange],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
+            gradient: LinearGradient(
+              colors: <Color>[Colors.amber, Colors.deepOrange],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
           ),
+          // Đây là tiêu đề của nhóm sản phẩm // This is title of group product
           child: Padding(
             padding: const EdgeInsets.only(left: 15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(Icons.hot_tub,color: this.color,),
-                      SizedBox(width: 20,),
-                      Text("Khuyến mại hót".toUpperCase(),style: TextStyle(color: this.color, fontWeight: FontWeight.w700),)
-                    ],
-                  )
-                ),
-                IconButton(
-                  padding: EdgeInsets.all(0),
-                  icon: Icon(Icons.keyboard_arrow_right,color: this.color,),
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(
+                      Icons.hot_tub,
+                      color: this.color,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "Khuyến mại hót".toUpperCase(),
+                      style: TextStyle(color: this.color, fontWeight: FontWeight.w700),
+                    )
+                  ],
+                )),
+                Row(
+                  children: <Widget>[
+ButtonBar(
+
+  children: <Widget>[
+    Text("Xem thêm"),
+    Icon(Icons.keyboard_arrow_right)
+  ],
+)
+                  ],
                 ),
               ],
             ),
           ),
-        )
+        ),
       ],
     );
   }
