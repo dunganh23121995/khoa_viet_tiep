5cbd134effd88269cb4c1299c1695837145ad947
dunganh23121995 <dungca2312@gmail.com>
2020-07-20 10:53:28 +0700

    humh

--- lib/view/page/category_page.dart
+++ lib/view/page/category_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:khoaviettiep/publiccustom/card_a_product.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -30,22 +31,33 @@ class _CategoryPageState extends State with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Center(
-      child: Text("")
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: ListView.builder(itemBuilder: (context,index){
+              return Container(
+                child: Tab(
+                  icon: Image.asset("assets/images/01504-copy.png"),
+                  child: Text("Why"),
+                ),
+                height: 120,
+              )
+              ;
+            },
+            itemCount: 5,),
+          ),
+          Expanded(
+            flex: 8,
+            child:Center(
+              child:  Text("List danh sach"),
+            ),
+          )
+        ],
+      ),
     );
-//    return Column(
-//      children: <Widget>[
-//
-//        Container(
-//          child: Transform.scale(
-//            key: keyCategory,
-//            scale: 10.0,
-//            origin: Offset(animation.value, animation.value),
-//            transformHitTests: true,
-//            child: Icon(Icons.shop),
-//          ),
-//        ),
-//      ],
-//    );
   }
 }
