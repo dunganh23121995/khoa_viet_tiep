4d7a80a6456a98dd1e4bc3534af157ad9d143b03
dungca2312@gmail.com <Dung34161@nuce.edu.vn>
2020-07-30 00:40:12 +0700

    fdas

--- lib/view/products.dart
+++ lib/view/products.dart
@@ -29,34 +29,60 @@ class _ProductsShow extends State {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(),
-        body: StreamBuilder(
-          stream: ProductsBloc.Instance().productsStream,
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? GridView.builder(
-                itemCount: snapshot.data.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                    itemBuilder: (context, index) {
-                      return Container(
-                        child: CardProduct(
-                          width: double.infinity,
-                          ishot: snapshot.data[index]['ishot']=="true",
-                          isnew:snapshot.data[index]['updated']!=null?(DateTime.now().subtract(Duration(days: 365*2))).isBefore(DateTime.parse(snapshot.data[index]['updated'])):false,
-                          image: Image.network('${linkweb}${snapshot.data[index]['anhdaidien']}',fit: BoxFit.fitWidth,),
-                          name: snapshot.data[index]['title'],
-                          price: int.parse(snapshot.data[index]['giaban']),
-                          cost: int.parse(snapshot.data[index]['giathitruong']),
-                        ),
-                      );
-                    })
-                : LoadingIndicator(
-              indicatorType: Indicator.ballClipRotateMultiple,
-              color: AppTheme.colorOrange,
-            );
-          },
-        ));
+    return Material(
+      child: Scaffold(
+          appBar: AppBar(),
+          body: StreamBuilder(
+            stream: ProductsBloc.Instance().productsStream,
+            builder: (context, snapshot) {
+              return snapshot.hasData
+                  ? GridView.builder(
+                  itemCount: snapshot.data.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          child: Hero(
+                            tag: 'productdetails${index}',
+                            child: CardProduct(
+                              ontap: (){
+                                Navigator.of(context).push(MaterialPageRoute<void>(builder: (context){
+                                  //Builder child
+                                  return Scaffold(
+                                    appBar: AppBar(),
+                                    body: Hero(
+                                      tag: 'productdetails${index}',
+                                      child: CardProduct(
+                                        width:  MediaQuery.of(context).size.width,
+                                        height: MediaQuery.of(context).size.height,
+                                        ishot: snapshot.data[index]['ishot']=="true",
+                                        isnew:snapshot.data[index]['updated']!=null?(DateTime.now().subtract(Duration(days: 365*2))).isBefore(DateTime.parse(snapshot.data[index]['updated'])):false,
+                                        image: Image.network('${linkweb}${snapshot.data[index]['anhdaidien']}',fit: BoxFit.fitWidth,),
+                                        name: snapshot.data[index]['title'],
+                                        price: int.parse(snapshot.data[index]['giaban']),
+                                        cost: int.parse(snapshot.data[index]['giathitruong']),
+                                      ),
+                                    ),
+                                  );
+                                }));
+                              },
+                              width: double.infinity,
+                              ishot: snapshot.data[index]['ishot']=="true",
+                              isnew:snapshot.data[index]['updated']!=null?(DateTime.now().subtract(Duration(days: 365*2))).isBefore(DateTime.parse(snapshot.data[index]['updated'])):false,
+                              image: Image.network('${linkweb}${snapshot.data[index]['anhdaidien']}',fit: BoxFit.fitWidth,),
+                              name: snapshot.data[index]['title'],
+                              price: int.parse(snapshot.data[index]['giaban']),
+                              cost: int.parse(snapshot.data[index]['giathitruong']),
+                            ),
+                          ),
+                        );
+                      })
+                  : LoadingIndicator(
+                indicatorType: Indicator.ballClipRotateMultiple,
+                color: AppTheme.colorOrange,
+              );
+            },
+          )),
+    );
   }
 }
